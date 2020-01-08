4331a67c91da2c8aec665702928520407c804d26
Octopus-free <ivashkinrg@gmail.com>
2020-01-08 23:40:43 +0300

    Initial

---
+++ final_dict.py
@@ -0,0 +1,15 @@
+import pprint
+
+hh_vacancies = {}
+
+hh_vacancies = {
+                'vacancy_text': 'python',
+                'count': 0,
+                'requirements': {
+                                'name': 'python',
+                                'skill_count': 0,
+                                'skill_percent': 0
+                }
+}
+
+pprint.pprint(hh_vacancies)
